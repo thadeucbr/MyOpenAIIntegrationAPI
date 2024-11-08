08f3a2c193307618406f722404c20c3d1cf1373b
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-11-08 14:32:43 -0300

    add ModelsController.cs

---
+++ MyOpenAIIntegrationAPI/Controllers/ModelsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using MyOpenAIIntegrationAPI.Models;
+using MyOpenAIIntegrationAPI.TemplateClass;
+
+namespace MyOpenAIIntegrationAPI.Controllers;
+
+[Route("v1/models")]
+[ApiController]
+public class ModelsController : ControllerTemplate
+{
+    public ModelsController(HttpClient httpClient) : base(httpClient)
+    {
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> GetModels()
+    {
+        var response = await _httpClient.GetAsync("https://api.openai.com/v1/models");
+        if (response.IsSuccessStatusCode)
+        {
+            var modelsData = JsonSerializer.Deserialize<ModelsResponse>(await response.Content.ReadAsStringAsync());
+                var models = modelsData.data.Select(m => m.id).ToList();
+            return Ok(models);
+        }
+
+        return StatusCode((int)response.StatusCode, await response.Content.ReadAsStreamAsync());
+    }
+}
